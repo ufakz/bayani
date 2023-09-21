0d23bb49158ba901504fa691c0c09e49eda2b6d3
Farouq Umar <ufakz99@gmail.com>
2023-09-21 14:18:35 +0300

    feat: process text and chunks

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
 import streamlit as st
 from dotenv import load_dotenv
+from pypdf import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    
+    return text
+
+def get_text_chunks(raw_text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+
+    chunks = text_splitter.split_text(raw_text)
+
+    return chunks
 
 def main():
     load_dotenv()
@@ -10,8 +33,18 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDFs here and click on 'Process'",accept_multiple_files=True)
-        st.button("Process")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'",accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get text chunks
+                text_chunks = get_text_chunks(raw_text)
+                # create vector store
+            
+
 
 
 if __name__ == '__main__':
